d5d68e1e980dfcc93beabe755889f80b7aae4b8e
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-07-28 14:51:21 -1000

    completed

---
+++ sorting/mergesort.js
@@ -0,0 +1,29 @@
+var a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+function mergeS(x) {
+  if (x.length < 2) {
+    return x;
+  } else {
+    var mid = Math.floor(x.length / 2);
+    var left = x.slice(0, mid);
+    var right = x.slice(mid, x.length);
+    return merge(mergeS(left), mergeS(right));
+  }
+}
+function merge(left, right) {
+  var newX = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      newX.push(left.shift());
+    } else {
+      newX.push(right.shift());
+    }
+  }
+  while (left.length) {
+    newX.push(left.shift());
+  }
+  while (right.length) {
+    newX.push(right.shift());
+  }
+  return newX;
+}
+console.log(mergeS(a));
